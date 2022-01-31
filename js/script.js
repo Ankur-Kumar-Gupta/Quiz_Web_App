45610123e323a6297be4e565548d684e0fcb0afa
Ankur <50777967+Ankur-Kumar-Gupta@users.noreply.github.com>
2022-02-01 05:03:00 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Code by Ankur Kumar Gupta ..... */
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
