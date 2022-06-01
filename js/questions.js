1fad73c11fe5df214d630d1165e42c5f7572f095
Ankur <50777967+Ankur-Kumar-Gupta@users.noreply.github.com>
2022-06-01 05:30:33 +0530

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -3,222 +3,57 @@
 let questions = [
   {
   numb: 1,
-  question: "The concept that states that every transaction has at least two accounts involved in recording a transaction is?",
-  answer: "Dual Aspect",
+  question: "HTML stands for?",
+  answer: "HyperText Markup Language",
   options: [
-    "Matching",
-    "Dual Aspect",
-    "Full Disclosure",
-    "Conservatism"
+    "HyperDrive Markup Language",
+    "HyperText Markup Language",
+    "HyperText Markdown Language",
+    "HiperTool Markup Language"
   ]
 },
   {
   numb: 2,
-  question: "Identify the Accounting Principle which includes the cost of acquisition that does not change year after year?",
-  answer: "Cost Concept",
+  question: "CSS Stands for?",
+  answer: "Cascade Style Sheet",
   options: [
-    "Cost Concept",
-    "Revenue Recognition",
-    "Accounting Period",
-    "Consistency"
+    "Cascade Style Sheet",
+    "Comman Style Sheet",
+    "Cascade Sheet Style",
+    "Continuous Sleep Syndrome"
   ]
 },
   {
   numb: 3,
-  question: "The revenue is assumed to be realised when a legal right to receive it arises under the?",
-  answer: "Realisation Concept",
+  question: "Who is the father of Computers?",
+  answer: "Charles Babbage",
   options: [
-    "Matching Concept",
-    "Realisation Concept",
-    "Cost concept",
-    "Dual Aspect"
+    "James Gosling",
+    "Charles Babbage",
+    "Bjarne Stroustrup",
+    "Charles Darwin"
   ]
 },
   {
   numb: 4,
-  question: "Concept that anticipates the Loss and not the Profit refers to?",
-  answer: "Conservatism",
+  question: "What is the full form of CPU",
+  answer: "Central Processing Unit",
   options: [
-    "Consistency",
-    "Full Disclosure",
-    "Conservatism",
-    "Matching"
+    "Computer Processing Unit",
+    "Computer Principle Unit",
+    "Control Processing Unit",
+    "Central Processing Unit"
   ]
 },
   {
   numb: 5,
-  question: "Which principle does implies when the closing stock is valued at the cost or market value whichever is less?",
-  answer: "conservatism",
+  question: "Which of the following is the smallest unit of data in a computer?",
+  answer: "Bit",
   options: [
-    "Cost",
-    "conservatism",
-    "Materiality",
-    "consistency"
-  ]
-},
-  {
-  numb: 6,
-  question: "Which concept does the Income Tax act refer under The Companies Act that requires that the income statement should be prepared annually? ",
-  answer: "Cost",
-  options: [
-    "Cost",
-    "conservatism",
-    "Materiality",
-    "consistency"
-  ]
-},
-  {
-  numb: 7,
-  question: "Identify the concept that requires the documentary evidence for the accounting transaction which is supported by verifiable documents or vouchers ",
-  answer: "Objectivity",
-  options: [
-    "Materiality",
-    "Objectivity",
-    "Full Disclosure ",
-    "consistency"
-  ]
-},
-  {
-  numb: 8,
-  question: "Under which accounting concept the personal transactions of the owner are not recorded in the books of the business, unless it involves inflow or outflow of business funds ",
-  answer: "Business Entity",
-  options: [
-    "Materiality",
-    "Business Entity",
-    "Full Disclosure ",
-    "Dual Aspect"
-  ]
-},
-  {
-  numb: 9,
-  question: "Identify the assumption of accounting that provides the very basis for showing the value of assets in the Balance Sheet ",
-  answer: "Going Concern Concept",
-  options: [
-    "Materiality",
-    "Business Entity",
-    "Full Disclosure ",
-    "Going Concern Concept"
-  ]
-},
-  {
-  numb: 10,
-  question: "According to which accounting concept, the capital introduced while commencement of the business is considered as the liability of the firm?",
-  answer: "Business Entity",
-  options: [
-    "Matching",
-    "Business Entity",
-    "Full Disclosure ",
-    "Going Concern Concept"
-  ]
-},
-  {
-  numb: 11,
-  question: "The concept that states the revenue and the expenses incurred to earn the revenue must belong to the same accounting period for ascertaining profit/loss for that year",
-  answer: "Matching",
-  options: [
-    "Matching",
-    "Business Entity",
-    "Full Disclosure ",
-    "Going Concern Concept"
-  ]
-},
-  {
-  numb: 12,
-  question: "The credit sales are treated as revenue on the day sales are made and not when money is received from the buyer refers to the: ",
-  answer: "Realisation",
-  options: [
-    "Materiality",
-    "Realisation",
-    "Money Measurement",
-    "Going Concern Concept"
-  ]
-},
-  {
-  numb: 13,
-  question: "The accounting data does not reflect the True and fair view of the firm as the heterogeneous value of two different assets that has been purchased in two different year, can’t be clubbed together is the limitation of the concept of: ",
-  answer: "Money Measurement",
-  options: [
-    "Materiality",
-    "Business Entity",
-    "Money Measurement",
-    "Going Concern Concept"
-  ]
-},
-  {
-  numb: 14,
-  question: "The concept that states that the reasonable material facts must be disclosed through the financial statement and the informing notes is: ",
-  answer: "Materiality",
-  options: [
-    "Materiality",
-    "Business Entity",
-    "Full Disclosure ",
-    "Going Concern Concept"
-  ]
-},
-  {
-  numb: 15,
-  question: "Identify the Accounting principle which express the fundamental Accounting Equations",
-  answer: "Dual Aspect",
-  options: [
-    "Materiality",
-    "Dual Aspect",
-    "Full Disclosure ",
-    "Going Concern Concept"
-  ]
-},
-  {
-  numb: 16,
-  question: "State the principle which enables the comparison of the financial results inter- firm as well as intra firm year after year: ",
-  answer: "Consistency",
-  options: [
-    "Materiality",
-    "Business Entity",
-    "Full Disclosure ",
-    "Consistency"
-  ]
-},
-  {
-  numb: 17,
-  question: "ABC Ltd. received an advance of Rs 50,000 on sale in the month of December 2019 for the sales made in the month of May 2020. The sale should be recognized in the financial year",
-  answer: "2020-21",
-  options: [
-    "2019-20",
-    "2020-21",
-    "in both the years",
-    "should not be recorded anywhere"
-  ]
-},
-  {
-  numb: 18,
-  question: "A Firm purchased Building for Rs 1,00,000. But at the time of preparing the final accounts, its market value was Rs 5,00,000. However, in the books of accounts, it was shown at Rs 90,000 after deduction of depreciation of Rs 10,000. Which concept is highlighted here?",
-  answer: "Cost Concept",
-  options: [
-    "Materiality",
-    "Cost Concept",
-    "Full Disclosure ",
-    "Going Concern Concept"
-  ]
-},
-  {
-  numb: 19,
-  question: "The owner of Zebra Ltd., purchases a machine and records it at its original cost. The depreciation of the machine will be distributed over its useful life. Which concept is highlighted here?",
-  answer: "Going Concern Concept",
-  options: [
-    "Materiality",
-    "Business Entity",
-    "Full Disclosure ",
-    "Going Concern Concept"
-  ]
-},
-  {
-  numb: 20,
-  question: "The firm purchased the machine costing Rs.100,000 and charged depreciation @ 10% according to the Straight line Method. After 2 years they switch to the Written down value method. At the time of sale of machine they calculated the depreciation as per the SLM. Which accounting principle is violated? ",
-  answer: "Consistency",
-  options: [
-    "Materiality",
-    "Business Entity",
-    "Consistency",
-    "Going Concern Concept"
+    "Nibble",
+    "Bit",
+    "Kb",
+    "Byte"
   ]
 }
 ];
